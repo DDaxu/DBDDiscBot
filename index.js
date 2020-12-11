609c9da48b2a7283f88e6ffd22afa65234c17892
DDaxu <akselikoskela93@gmail.com>
2020-12-11 15:42:52 +0200

    Finished bot functions and some clean up done

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 const token = process.env.TOKEN;
 
 const client = new Discord.Client();
-//console.log(process.env.TOKEN);
 client.on('message', (msg) => {
 
     //command handler
@@ -15,36 +14,58 @@ client.on('message', (msg) => {
     const command = args.shift().toLowerCase();
     //commands
     if(command === 'hello') {
-        msg.channel.send(`${msg.author} Terve!`);
-        JSONdeleteRule();
+        msg.channel.send(`${msg.author} Hello!`);
+    }
+    if(command === 'commands'){
+        //Making nice looking embed message for all commands we have
+        const embed = new Discord.MessageEmbed()
+            .setColor('#D20101')
+            .setTitle('Dead By Daylight drinking game bot commands')
+            .setDescription('This is bot for handling drinking game rules. It can for example add new rules, modify them or even delete them.')
+            .addField('\u200B','\u200B')
+            .addField('hello', 'Says hello to you', true)
+            .addField('commands', 'Give command list to you', true)
+            .addField('\u200B','\u200B')
+            .addFields(
+                {name: 'dbd', value: 'write dbd and argument after that. (for example !dbd write)' },
+                {name: 'read', value: 'Send rules as a message to this channel', inline:true },
+                {name: 'write', value: 'Write new rule to rule list', inline:true },
+                {name: 'modify', value: 'Modify given rule' },
+                {name: 'delete', value: 'Delete given rule', inline:true },
+                {name: 'file', value: 'Gives you downloadable text file', inline:true },
+            )
+            .setTimestamp();
+            msg.channel.send(embed);
     }
     //Drunk by daylight rules and rules management
     if(command === 'dbd'){
         if(!args.length){
             return msg.channel.send("Give command please.");
+        //Send list of rules to the channel
         } else if(args[0] === 'read'){
             var string = JSONtoString();
             msg.channel.send(string); 
+        //Add new rule to the channel
         } else if(args[0] === 'write'){
-            msg.channel.send('Lisätään uusi sääntö. Kirjoita seuraavat tiedot seuraavassa järjestyksessä:\nsääntö \nkulien määrä');
-            var y = 1;
+            msg.channel.send('Let`s add new rule to the channel. Please write rule description first.');
+            var calc = 1;
             var kuli = 0;
             var teksti = '';
+            //Collectors job is to collect multiple answers from the user and it is used multiple times in this program
             const collector = new Discord.MessageCollector(msg.channel,m => m.author.id === msg.author.id);
             collector.on('collect', (message, col) =>{
-                if(y === 1){
+                if(calc === 1){
                     teksti = message.content;
-                    msg.channel.send("sääntö on: " + teksti +"\n\n Anna seuraavaksi kulien määrä");
-                }else if (y === 2){
+                    msg.channel.send("Rule is: " + teksti +"\n\nGive amount of kulis");
+                }else if (calc === 2){
                     kuli = message.content;
-        
                     collector.stop();
                 }
-                y++;
+                calc++;
             });
+            //Shutting collector and now colletor starts to process the given information
             collector.on('end', collected =>{
-                msg.channel.send("sääntö on: " + teksti + "\nkulimäärä on: " + kuli);
-                console.log(parseInt(kuli, 10));
+                msg.channel.send("Rule is: " + teksti + "\nkuli amount is: " + kuli);
                 
                 if(isNaN(kuli) == false){
                     JSONnewRule(teksti, kuli);
@@ -53,28 +74,29 @@ client.on('message', (msg) => {
                 }
     
             });
+        //Modify the spesific rule
         } else if(args[0] === 'modify'){
             msg.channel.send('Modify rule with rule ID. Give rule ID you want to modify.');
-            var y = 1;
+            var calc = 1;
             var ruleId = 0;
             var rule = "";
             var kuli = 0;
             const collector = new Discord.MessageCollector(msg.channel,m => m.author.id === msg.author.id);
             collector.on('collect', (message, col) => {
-                if(y == 1){
+                if(calc == 1){
                     ruleId = message.content;
                     msg.channel.send("The rule ID you want to change is: " + ruleId + ". Now write new rule.");
-                } else if(y == 2){
+                } else if(calc == 2){
                     rule = message.content;
                     msg.channel.send("The new rule is: " + rule + ". Now write kuli amount.");
-                } else if(y == 3){
+                } else if(calc == 3){
                     kuli = message.content;
                     collector.stop();
                 }
-                y++;
+                calc++;
             });
             collector.on('end', collected =>{
-                msg.channel.send("ruleID is: " + ruleId + "\nnew rule is:  " + rule + "\nnew kuli amount is: " + kuli);
+                msg.channel.send("RuleID is: " + ruleId + "\nnew rule is:  " + rule + "\nnew kuli amount is: " + kuli);
                 if(isNaN(kuli) == false){
                     msg.channel.send(JSONmodifyRule(ruleId, rule, kuli));
                 } else {
@@ -82,77 +104,140 @@ client.on('message', (msg) => {
                 }
 
             });
-        }  else if(args[0] === 'tiedosto'){
+        //Delete given rule by ID
+        } else if(args[0] === 'delete'){
+            msg.channel.send('Give rule ID you want to delete');
+            var calc = 1;
+            var ruleID = 0;
+            var answer = "";
+            var deleteBool = 0;
+            const collector = new Discord.MessageCollector(msg.channel,m => m.author.id === msg.author.id);
+            collector.on('collect', (message, col) => {
+                if(calc == 1){
+                    ruleID = message.content;
+                    //Extra question about deleting for it´s severity
+                    msg.channel.send("The rule you want to delete is: " + JSONgetRule(ruleID)+ " Do you want to proceed? (Y/N)");
+                } else if(calc == 2){
+                    answer = message.content;
+                    answer = answer.toLowerCase();
+                    if(answer == "y" || answer == "yes"){
+                        msg.channel.send("Deleting the rule...");
+                        deleteBool = 1;
+                        collector.stop();
+                    } else {
+                        msg.channel.send("Deleting process is cancelled.");
+                        deleteBool = 0;
+                        collector.stop();
+                    }
+                }
+                calc++;
+            });
+            collector.on('end', collected =>{
+                if(deleteBool == 1){
+                    JSONdeleteRule(ruleID);
+                    msg.channel.send("Rule has been deleted.");
+                } 
+            });
+        //Give rules as a text file
+        }  else if(args[0] === 'file'){
             var string = JSONtoString();
-            fs.writeFile('./test.txt', string, function(err) {
+            fs.writeFile('./DBDrules.txt', string, function(err) {
                 if(err) return console.log(err);
                 
             });
-            const attachment = new MessageAttachment('./test.txt');
+            const attachment = new MessageAttachment('./DBDrules.txt');
             msg.channel.send(`${msg.author}`, attachment);
         } 
     }
 
 });
+
+/* 
+ * Functions for handling JSON files how we want.
+ * Idea is to make top code more simple to read.
+ * For the future: These codes can be merged as one function propably.
+ */
+
+ //JSON file to a String. We want to send one big message to the channel instead of multiple messages
 function JSONtoString(){
     var content = fs.readFileSync(rules);
-    var a = JSON.parse(content);
+    var ruleList = JSON.parse(content);
     var string = '';
-    a.forEach(function(x) {
+    ruleList.forEach(function(x) {
         string += x.ruleID + ". " + x.rule + " = kulis " + x.amount + '\n';
     });
     return string;
 }
+// Get one spesific rule for deleting as a text line
+function JSONgetRule(id){
+    var content = fs.readFileSync(rules);
+    var ruleList = JSON.parse(content);
+    var string = '';
+    ruleList.forEach(function(x) {
+        if(x.ruleID == id){
+            string += x.ruleID + ". " + x.rule + " = kulis " + x.amount + '\n';
+        }
+    });
+    return string;
+}
+//Add new rule to the end of the rule list
 function JSONnewRule(newRule, kuliNumber){
     var content = fs.readFileSync(rules);
-    var JSONrule = JSON.parse(content);
+    var ruleList = JSON.parse(content);
     var lastNumber;
     
-    JSONrule.forEach(function(x) {
+    ruleList.forEach(function(x) {
         lastNumber = x.ruleID; 
      });
      lastNumber = lastNumber + 1;
-    JSONrule.push({"ruleID": lastNumber,"rule": newRule, "amount": parseInt(kuliNumber, 10)});
-    fs.writeFile('./rules.json',JSON.stringify(JSONrule, null, 2),'utf8', function(err) {
+     ruleList.push({"ruleID": lastNumber,"rule": newRule, "amount": parseInt(kuliNumber, 10)});
+    fs.writeFile('./rules.json',JSON.stringify(ruleList, null, 2),'utf8', function(err) {
         if(err) return console.log(err);
         
     });
 }
-
-function JSONdeleteRule(){
+//Delete rule by ID
+function JSONdeleteRule(id){
     var content = fs.readFileSync(rules);
-    var JSONrule = JSON.parse(content);
-    i = 0;
-    JSONrule.forEach(function(x){
-       // console.log("Ennen poistoa: " + x.ruleID + " " +x.rule + " " + x.amount);
-       console.log(x.ruleID);
-        if(x.ruleID == 28){
-           // console.log(i); 
+    var ruleList = JSON.parse(content);
+    //calculator for making new order for IDs
+    var i = 1;
+    ruleList.forEach(function(x){
+        if(x.ruleID == id){
             var y = x.ruleID;
-            console.log(y);
-            JSONrule.splice(y, 1);
-           // console.log("Poiston jälkeen: " + x.ruleID + " " +x.rule + " " + x.amount);
+            if(isNaN(y) == false){
+                console.log(x.rule);
+                y = y - 1;
+                ruleList.splice(y, 1);
+            }
         }
+    });
+
+    //set ID:s back to normal when one rule is removed.
+    ruleList.forEach(function(x){
+        x.ruleID = i;
         i++;
     });
-    fs.writeFile('./rules.json', JSON.stringify(JSONrule, null, 2),'utf8',function(err){
+    fs.writeFile('./rules.json', JSON.stringify(ruleList, null, 2),'utf8',function(err){
         if(err) return console.log(err);
         });
 }
+//Modify spesific rule by ID. parameters are new rule and kuli amount
 function JSONmodifyRule(id, newRule, newKuli){
     var content = fs.readFileSync(rules);
-    var JSONrule = JSON.parse(content);
+    var ruleList = JSON.parse(content);
     var idExists = 0;
     var complete = "";
-    JSONrule.forEach(function(x) {
+    ruleList.forEach(function(x) {
         if(x.ruleID == id){
             x.rule = newRule;
             x.amount = parseInt(newKuli, 10);
             idExists = 1;
         }
     });
+    //Extra check for ID and if everything is OK, we can proceed.
     if(idExists == 1){
-        fs.writeFile('./rules.json', JSON.stringify(JSONrule, null, 2),'utf8',function(err){
+        fs.writeFile('./rules.json', JSON.stringify(ruleList, null, 2),'utf8',function(err){
         if(err) return console.log(err);
         });
         complete = "Rule was changed. try !dbd read to see changes.";
@@ -164,7 +249,7 @@ function JSONmodifyRule(id, newRule, newKuli){
 }
 
 client.on('ready', () => {
-    console.log('Robotti avasi oven');
+    console.log('Robobot is online.');
 });
 
 client.login(token);
